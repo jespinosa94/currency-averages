559e41db2a7556a77930e32fbf4cd2f253d353b7
Jorge <jorge.espinosacam@gmail.com>
2021-12-11 18:16:41 +0100

    Refactor remove files from downloads directory

--- currency_averages/toolkit.py
+++ currency_averages/toolkit.py
@@ -6,13 +6,18 @@ import pprint
 def read_config(path):
     with open(path, "r") as f:
         return yaml.safe_load(f)
-    
-def clear_downloads_path(path):
-    """Delete all FXTOP files
-    """
+
+def get_fxtop_files(downloads_path):
     file_pattern = re.compile("^FXTOP.*")
-    file_list = os.listdir(path)
+    file_list = os.listdir(downloads_path)
     file_list_filtered = list(filter(file_pattern.match, file_list))
-    for file in file_list_filtered:
+    return file_list_filtered
+    
+    
+def remove_files_from(path):
+    """Delete all files in list
+    """
+    file_list = get_fxtop_files(path)
+    for file in file_list:
         f = os.path.join(path, file)
         os.remove(f)
