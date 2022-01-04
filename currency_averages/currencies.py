0db8680afa26d62006b986775096fabcbad0d456
Jorge <jorge.espinosacam@gmail.com>
2022-01-04 20:36:07 +0100

    Create Currency class load and save backups

---
+++ currency_averages/currencies.py
@@ -0,0 +1,31 @@
+import pandas as pd
+from datetime import datetime
+import os
+
+class Currencies:
+    def __init__(self):
+            self.backup_dir = "bck/"
+            self.currencies = self.load_backup()
+            
+    def load_backup(self):
+        try:
+            file_list = os.listdir(self.backup_dir)
+            file_list.sort(reverse=True)
+            last_file = file_list[0]
+            backup = pd.read_csv(os.path.join(self.backup_dir, last_file))
+            return backup
+        except:
+            print("No backup file found, creating empty Dataframe")
+            return pd.DataFrame()
+        
+        
+    def append_currency(self, df_new_currency):
+        self.currencies = pd.concat([self.currencies, df_new_currency],
+                                    axis=1)
+        print("Merged dataframes")
+        
+    def save_backup(self):
+        date_formatted = datetime.now().strftime("%Y%m%d_%H%M")
+        self.currencies.to_csv(self.backup_dir+"currencies"+date_formatted+".csv",
+                               sep=";")
+        print("Backup created")
--- currency_averages/main.py
+++ currency_averages/main.py
@@ -1,9 +1,15 @@
 import web_extractor as we
+import currencies as cur
 import toolkit
 
 
 config = toolkit.Config()
+currencies = cur.Currencies()
 
 # web_extractor = we.WebExtractor()
 # web_extractor.extract_historical_data("USD")
-toolkit.load_historical_data(config.downloads_folder)
+df_new_currency = toolkit.load_historical_data(config.downloads_folder)
+currencies.append_currency(df_new_currency)
+currencies.save_backup()
+
+print("Finished")
